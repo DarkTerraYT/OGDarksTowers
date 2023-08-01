a3018356799cff1f29f8c5e523f22628f6842ecd
DarkTerraYT <liamwolfguy@gmail.com>
2023-07-31 21:20:34 -0400

    Add project files.

---
+++ DarksTowers.cs
@@ -0,0 +1,13 @@
+using MelonLoader;
+using BTD_Mod_Helper;
+using DarksTowers;
+
+[assembly: MelonInfo(typeof(DarksTowers.DarksTowers), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace DarksTowers;
+
+public class DarksTowers : BloonsTD6Mod
+{
+    //Nothing for now!
+}
