c87fac7fdc42de1b2eb3771c912552eeed3a63ce
DarkTerraYT <liamwolfguy@gmail.com>
2023-07-31 21:45:21 -0400

    Updated ModHelperData

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,12 +2,12 @@ namespace DarksTowers;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "37.3";
+    public const string WorksOnVersion = "38.1";
     public const string Version = "1.0.0";
     public const string Name = "DarksTowers";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Adds custom towers to the game. Currently only has the plasma monkey.";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "DarksTowers"; // TODO add your repo name here, also in the download url in README.md
 }
