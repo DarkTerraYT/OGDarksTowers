f0e16c0d7af513871532a5766dc18eb218ee07d1
DarkTerraYT <liamwolfguy@gmail.com>
2023-08-16 14:46:59 -0400

    Updated Mod Description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "2.0.0 BETA V2";
     public const string Name = "DarksTowers";
 
-    public const string Description = "Adds custom towers to the game. Currently only has the plasma monkey.";
+    public const string Description = "Adds custom towers to the game. Currently adds the monkey of light and the plasma monkey.";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "DarksTowers"; // TODO add your repo name here, also in the download url in README.md
