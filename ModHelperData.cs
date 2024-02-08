14766a9c8060b3fc8a83fdc904a3a70f6e7846aa
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2024-02-07 21:57:58 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "4.0.1";
     public const string Name = "DarksTowers";
 
-    public const string Description = "Adds custom towers to the game. Currently adds the monkey of light, plasma monkey, and cyber monkey. \n I plan on adding one more new tower, if you have any ideas for it join the discord: xegnVEBRuE";
+    public const string Description = "Adds custom towers to the game. Currently adds the monkey of light, plasma monkey, and cyber monkey. \n I plan on adding one more new tower, if you have any ideas for it join the discord: xegnVEBRuE. Contact me on discord: @lerpmcgerk";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "DarksTowers"; // TODO add your repo name here, also in the download url in README.md
