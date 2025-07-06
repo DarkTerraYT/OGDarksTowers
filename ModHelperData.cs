ac508305a51aac5b840b93afb4494d85c7b13567
liamw <liamwolfguy@gmail.com>
2025-07-06 17:02:13 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,13 +2,13 @@ namespace DarksTowers;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "46.0";
-    public const string Version = "4.0.4";
-    public const string Name = "DarksTowers";
+    public const string WorksOnVersion = "49.0";
+    public const string Version = "4.0.5";
+    public const string Name = "OG Dark's Towers";
 
     public const string Description = "Adds custom towers to the game. Currently adds the monkey of light, plasma monkey, and cyber monkey. \n I plan on adding one more new tower, if you have any ideas for it join the discord: xegnVEBRuE. Contact me on discord: @lerpmcgerk";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "DarksTowers"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "OGDarksTowers"; // TODO add your repo name here, also in the download url in README.md
     public const string Author = "DarkTerraYT";
 }
